383ba5e78faf9f6dcfd02d499ec40c44e93c0142
nehaprabhu1718 <nehaprabhu1718@gmail.com>
2024-08-13 21:05:28 -0700

    feat: basic update listener

--- src/main/java/com/example/kafkaexample/events/listeners/UpdateEventListener.java
+++ src/main/java/com/example/kafkaexample/events/listeners/UpdateEventListener.java
@@ -1,13 +1,19 @@
 package com.example.kafkaexample.events.listeners;
 
 import com.example.kafkaexample.config.MongoConfig;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.changestream.FullDocument;
+import org.bson.conversions.Bson;
+import java.util.List;
+import com.mongodb.client.ChangeStreamIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.bson.Document;
+
+import java.util.Arrays;
 
 public class UpdateEventListener {
 
@@ -17,17 +23,34 @@ public class UpdateEventListener {
     @Value("${spring.data.mongodb.collection}")
     final public String collectionName;
 
+    @Autowired
+    final public MongoClient mongoClient;
+
     final public MongoCollection<Document> collection;
 
-    public UpdateEventListener(final MongoClient mongoClient, final MongoTemplate mongoTemplate, String database, String collectionName) {
+    public UpdateEventListener(final MongoClient mongoClient, String database, String collectionName) {
         this.database = database;
         this.collectionName = collectionName;
 
         this.mongoClient = mongoClient;
-        this.mongoTemplate = mongoTemplate;
-
         this.collection = mongoClient.getDatabase(database).getCollection(collectionName);
     }
 
+    public void listenToChangeEvent() {
+
+        List<Bson> pipeline = Arrays.asList(
+            Aggregates.match(Filters.in("operationType", Arrays.asList("insert", "update", "replace")))
+        );
+
+        ChangeStreamIterable<Document> changeStream = collection.watch(pipeline).fullDocument(FullDocument.UPDATE_LOOKUP);
+
+        changeStream.forEach(change -> {
+            processChange(change.getFullDocument());
+        });
+    }
+
+    public void processChange(Document fullChange) {
+        System.out.println(fullChange);
+    }
 
 }
